c258dbf728568813fb1516d9ab37f1acf8f2f8cc
Ken Harlbar <98645847+KenHarlbar@users.noreply.github.com>
2023-04-06 11:02:48 +0100

    Update affine_cipher.py

--- affine_cipher.py
+++ affine_cipher.py
@@ -111,7 +111,7 @@ for slope in slopes:
         for intercept in range(27):
 
         	response = str(decrypt(s2, slope, intercept))
-            if has_readable_words(response):
-                print("".join([d1.get(c, c) for c in s1]), end=". ")
-                print(response)
+                if has_readable_words(response):
+                    print("".join([d1.get(c, c) for c in s1]), end=". ")
+                    print(response)
         	continue
